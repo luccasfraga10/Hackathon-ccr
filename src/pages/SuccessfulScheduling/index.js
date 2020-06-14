75202a49730869c7752d6f02c5bc980dd770c284
Luccas Fraga <luccas.fraga@ifood.com.br>
2020-06-14 18:44:25 -0300

    fix

--- src/pages/SuccessfulScheduling/index.js
+++ src/pages/SuccessfulScheduling/index.js
@@ -1,40 +1,55 @@
 import React from 'react';
 import IconSuccess from '../../assets/icons/icon-success.svg';
-import useStyles from './styles'
+import useStyles from './styles';
+import Button from '@material-ui/core/Button';
 import { Box, Paper, Typography } from '@material-ui/core';
 import distanceIcon from '../../assets/icons/icon-distance.svg';
+import history from '../../services/history';
 
 const SuccessfulScheduling = () => {
+
+  const handleNext = () => {
+    history.push('/trips');
+  };
+
   const classes = useStyles();
   return (
-    <Box p={2} pt={7}>
-      <img className={classes.iconSuccess} src={IconSuccess} alt=""/>
+    <>
+      <Box p={2} pt={7}>
+        <img className={classes.iconSuccess} src={IconSuccess} alt=""/>
 
-      <Typography className={classes.text}>Viagem agendada<br /> com sucesso</Typography>
+        <Typography className={classes.text}>Viagem agendada<br /> com sucesso</Typography>
 
 
-      <Paper>
-        <Box p={3}>
-          <Typography component="b" className={classes.paperTitle}>Detalhes da viagem</Typography>
-          <Typography component="span" className={classes.description}>Sua rota</Typography>
+        <Paper>
+          <Box p={3}>
+            <Typography component="b" className={classes.paperTitle}>Detalhes da viagem</Typography>
+            <Typography component="span" className={classes.description}>Sua rota</Typography>
 
-          <div className={classes.contentTravel}>
-            <img src={distanceIcon} alt="" className={classes.contentTravelImg} />
-            <div className={classes.contentTravelInfo}>
+            <div className={classes.contentTravel}>
+              <img src={distanceIcon} alt="" className={classes.contentTravelImg} />
+              <div className={classes.contentTravelInfo}>
 
-              {localStorage.getItem('info') && JSON.parse(localStorage.getItem('info')).map(item => (
-                <Typography key={item} component="span" className={classes.textTravel}>{item}</Typography>
-              ))}
+                {localStorage.getItem('info') && JSON.parse(localStorage.getItem('info')).map(item => (
+                  <Typography key={item} component="span" className={classes.textTravel}>{item}</Typography>
+                ))}
+              </div>
             </div>
-          </div>
 
-          <div className={classes.contentTravelDate}>
-            <Typography component="span" className={classes.textTravelDate}>Data da viagem <b>{localStorage.getItem('date')}</b></Typography>
-            <Typography component="span" className={classes.textTravelDate}>Horário da viagem <b>{localStorage.getItem('time')}</b></Typography>
-          </div>
-        </Box>
-      </Paper>
-    </Box>
+            <div className={classes.contentTravelDate}>
+              <Typography component="span" className={classes.textTravelDate}>Data da viagem <b>{localStorage.getItem('date')}</b></Typography>
+              <Typography component="span" className={classes.textTravelDate}>Horário da viagem <b>{localStorage.getItem('time')}</b></Typography>
+            </div>
+          </Box>
+        </Paper>
+      </Box>
+
+      <div className={classes.boxButton}>
+      <Button fullWidth variant="outlined" onClick={handleNext}>
+        Ir para viagens
+      </Button>
+      </div>
+    </>
   )
 }
 
