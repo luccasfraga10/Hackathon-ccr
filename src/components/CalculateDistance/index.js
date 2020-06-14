d2ef8575b3cc207fb67adfca8f6bb133816afade
Luccas Fraga <luccas.fraga@ifood.com.br>
2020-06-13 21:42:26 -0300

    test

--- src/components/CalculateDistance/index.js
+++ src/components/CalculateDistance/index.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react';
-import { getDistance } from 'geolib';
+
+import ApiMap from '../../services/map';
 
 const CalculateDistance = () => {
-  useEffect(() => {
-    const a = getDistance(
-      { latitude: -23.5353856, longitude: -46.8979554 },
-      { latitude: -23.4814376, longitude: -46.7458937 }
+  const getApi = async () => {
+    const response = await ApiMap.get(
+      'json?units=imperial&origins=-23.4814376,-46.7458937&destinations=-23.5353856,-46.8979554&key=AIzaSyCjqDVWvcWUMDNNEf9FZohN4cfk1ZHtTIY'
     );
 
-    console.log('aaa =>', a);
-    // https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=-23.4814376,-46.7458937&destinations=-23.5353856,-46.8979554&key=AIzaSyCjqDVWvcWUMDNNEf9FZohN4cfk1ZHtTIY
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getApi();
   }, []);
 
   return (
