cdf4988953f2980ed5707227c46724d06481a36a
Luccas Fraga <luccas.fraga@ifood.com.br>
2020-06-14 18:37:56 -0300

    SuccessfulScheduling

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Home from './pages/Home';
 import Trip from './pages/Trip';
+import SuccessfulScheduling from './pages/SuccessfulScheduling';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/trip" exact component={Trip} />
+      <Route path="/successful-scheduling" exact component={SuccessfulScheduling} />
     </Switch>
   );
 }
